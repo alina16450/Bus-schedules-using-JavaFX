ed476129a8228ad8de22d46e28a8c4da422e0572
Buliga Alina <161938026+alina16450@users.noreply.github.com>
2025-02-08 14:15:34 +0200

    Update Service.java

--- src/service/Service.java
+++ src/service/Service.java
@@ -1,11 +1,74 @@
 package service;
 
+import Domain.Routes;
 import repository.Repository;
+import repository.RoutesRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Service {
-    private Repository repo;
+    private RoutesRepository repo;
 
-    public Service(Repository repo) {
+    public Service(RoutesRepository repo) {
         this.repo = repo;
     }
+
+    public List<Routes> sortedRoutes() {
+        List<Routes> routes = new ArrayList<>();
+        for (Routes route : repo.findAll()) {
+            routes.add(route);
+        }
+        return routes.stream().sorted(Comparator.comparing(Routes::getDestinationCity).thenComparing(Routes::getDepartureTime)).collect(Collectors.toList());
+    }
+
+    public Set<String> getSources() {
+        Set<String> sources = new HashSet<>();
+        for (Routes route : repo.findAll()) {
+            sources.add(route.getSourceCity());
+        }
+        return sources;
+    }
+
+    public Set<String> getDestinations(String sourceCity) {
+        Set<String> destinations = new HashSet<>();
+        for (Routes route : repo.findAll()) {
+            if (route.getSourceCity().equals(sourceCity)) {
+                destinations.add(route.getDestinationCity());
+            }
+        }
+        return destinations;
+    }
+
+    public List<Routes> getRoutes(String sourceCity, String destinationCity) {
+        List<Routes> routes = new ArrayList<>();
+        for (Routes route : repo.findAll()) {
+            routes.add(route);
+        }
+        return routes.stream().filter(route -> {
+                    return route.getDestinationCity().equals(destinationCity) && route.getSourceCity().equals(sourceCity);
+                })
+                .sorted(Comparator.comparing(Routes::getDestinationCity).thenComparing(Routes::getDepartureTime)).collect(Collectors.toList());
+    }
+
+    public double bookingCalc(Routes bookedRoute, int numTickets) {
+        double price = 0;
+
+        for (Routes route : repo.findAll()) {
+            if (route == bookedRoute) {
+                int availableSeats = bookedRoute.getAvailableSeats();
+                if (numTickets > availableSeats) {
+                    throw new RuntimeException("Not enough available seats");
+                } else {
+
+                    bookedRoute.setAvailableSeats(availableSeats - numTickets);
+                    price = bookedRoute.getTicketPrice();
+                    repo.update(route.getId(), bookedRoute);
+                    return price * numTickets;
+                    }
+                }
+            }
+        return price;
+    }
+
 }
